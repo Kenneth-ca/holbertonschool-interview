18555047cf50af65395df8d68e2a7de28c031a20
Kenneth-ca <kenneth.ca95@gmail.com>
2020-10-20 21:30:24 +0000

    fix check with requests

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,17 +1,18 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+const input = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + input;
+
 request(url, async function (error, response, body) {
   if (error) {
-    return console.log(error);
+    console.log(error);
   } else {
-    const values = JSON.parse(body).values;
-    for (const val in values) {
+    const characters = JSON.parse(body).characters;
+    for (const i in characters) {
       const res = await new Promise((resolve, reject) => {
-        request(values[val], (error2, res, html) => {
-          if (error2) {
-            reject(error2);
+        request(characters[i], (error, res, html) => {
+          if (error) {
+            reject(error);
           } else {
             resolve(JSON.parse(html).name);
           }
